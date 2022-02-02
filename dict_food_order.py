f576b75d5250af110c5dd7f6d22c35bb719bc68e
ArturGR30 <95871668+ArturGR30@users.noreply.github.com>
2022-02-03 00:15:59 +0200

    Add files via upload

---
+++ dict_food_order.py
@@ -0,0 +1,23 @@
+# HW: interactive FOOD ORDER
+
+food = [
+    {
+        "name": "Pizza",
+        "price": {
+            "value": 100.00,
+            "unit": "MDL"
+        }
+    },
+    {
+        "name": "Salad",
+        "price": {
+            "value": 50.00,
+            "unit": "MDL"
+        }
+    }
+]
+print("Hello and Welcome in our Bistro!")
+
+menu = []
+for i in food:
+    print(i)
